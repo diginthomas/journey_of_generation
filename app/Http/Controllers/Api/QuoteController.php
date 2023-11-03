3a9cb551102aa92eb789990e364810124e64bfe7
Digin Thomas <63897737+diginthomas@users.noreply.github.com>
2023-11-04 00:39:01 +0530

    add api

---
+++ app/Http/Controllers/Api/QuoteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Repositories\CommonRepository;
+use Carbon\Carbon;
+
+class QuoteController extends Controller
+{
+    public function index(CommonRepository $commonRepo){
+        $quotes = $commonRepo->getQuotes()
+            ->where('created_at','>=',Carbon::today())
+            ->get();
+        return response()->json($quotes);    
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\QuoteController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('quotes',[QuoteController::class,'index']);
