03d877d6e9069b16f898a1d3f51af0d09bce9df9
Milan555 <milan@insidestorybox.com>
2023-11-09 10:12:25 +0530

    updations

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,6 @@ use App\Http\Controllers\Api\SocialLoginController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 // Route::group(['middleware' => 'auth:sanctum'], function () {
 //
 // });
